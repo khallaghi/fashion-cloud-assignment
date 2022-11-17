932e5e0905dde9027b3e3292f3c45a9f847d6320
Mohammad Khallaghi <md.khallaghi@gmail.com>
2022-11-17 18:54:11 +0330

    Put port number into .env file

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function setupServer() {
     const app = express();
     app.use(express.json());
     // TODO: put the port number in .env alongside username and password
-    app.listen(3000, () => {
-        console.log(`Server started at ${3000}`)
+    const port = process.env.PORT
+    app.listen(port, () => {
+        console.log(`Server started at ${port}`)
     });
     const routes = require('./routes/routes');
     app.use('/api', routes)
